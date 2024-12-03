e71f150b50f1fbd28819674bed8581a64f2ff6bf
Jakob Runge <github@runjak.codes>
2024-12-03 18:39:32 +0100

    solve 03-2

--- src/task_03.rs
+++ src/task_03.rs
@@ -42,8 +42,73 @@ fn first() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Do,
+    Dont,
+    Mul { a: N, b: N },
+}
+
+fn match_instructions(input: String) -> Vec<Instruction> {
+    let instruction_regex =
+        Regex::new(r"((?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\((?<a>\d+),(?<b>\d+)\)))")
+            .unwrap();
+
+    let captures = instruction_regex.captures_iter(&input);
+
+    captures
+        .filter_map(|capture| -> Option<Instruction> {
+            let i_do = capture.name("do");
+            let i_dont = capture.name("dont");
+            let i_mul = capture.name("mul");
+
+            match (i_do, i_dont, i_mul) {
+                (Some(_), _, _) => Some(Instruction::Do),
+                (None, Some(_), _) => Some(Instruction::Dont),
+                (None, None, _) => {
+                    let a = capture.name("a")?.as_str().parse::<N>().ok()?;
+                    let b = capture.name("b")?.as_str().parse::<N>().ok()?;
+
+                    Some(Instruction::Mul { a, b })
+                }
+            }
+        })
+        .collect()
+}
+
+fn filter_instructions(instructions: Vec<Instruction>) -> Vec<Mul> {
+    let mut result = Vec::new();
+    let mut do_enabled = true;
+
+    for instruction in instructions.iter() {
+        match instruction {
+            Instruction::Do => {
+                do_enabled = true;
+            }
+            Instruction::Dont => {
+                do_enabled = false;
+            }
+            Instruction::Mul { a, b } => {
+                if do_enabled {
+                    result.push(Mul { a: *a, b: *b });
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+fn solution_2(path: String) -> Result<N, Box<dyn Error>> {
+    let input = read_input(path)?;
+    let muls = filter_instructions(match_instructions(input));
+
+    Ok(muls.iter().map(|Mul { a, b }| -> N { (*a) * (*b) }).sum())
+}
+
 fn second() -> Result<(), Box<dyn Error>> {
-    println!("{}", 0);
+    let wanted = solution_2("./inputs/03/input.txt".to_owned())?;
+    println!("{}", wanted);
     Ok(())
 }
 
@@ -56,7 +121,10 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    use super::{match_muls, read_input, solution_1, Mul, N};
+    use super::{
+        filter_instructions, match_instructions, match_muls, read_input, solution_1, solution_2,
+        Instruction, Mul, N,
+    };
 
     #[test]
     fn finds_example_muls() {
@@ -77,6 +145,41 @@ mod tests {
     fn calculates_first_example() {
         let actual = solution_1("./inputs/03/example.txt".to_owned()).unwrap();
         let expected: N = 161;
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn finds_instructions() {
+        let input = read_input("./inputs/03/example2.txt".to_owned()).unwrap();
+        let actual = match_instructions(input);
+
+        let expected = Vec::from([
+            Instruction::Mul { a: 2, b: 4 },
+            Instruction::Dont,
+            Instruction::Mul { a: 5, b: 5 },
+            Instruction::Mul { a: 11, b: 8 },
+            Instruction::Do,
+            Instruction::Mul { a: 8, b: 5 },
+        ]);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn finds_enabled_muls() {
+        let input = read_input("./inputs/03/example2.txt".to_owned()).unwrap();
+        let actual = filter_instructions(match_instructions(input));
+
+        let expected = Vec::from([Mul { a: 2, b: 4 }, Mul { a: 8, b: 5 }]);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculates_second_example() {
+        let actual = solution_2("./inputs/03/example2.txt".to_owned()).unwrap();
+        let expected: N = 48;
 
         assert_eq!(actual, expected);
     }
