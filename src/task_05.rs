a6ca3b961d0a60364f8931cb917d7758730bbcd2
Jakob Runge <github@runjak.codes>
2024-12-05 22:42:47 +0100

    solve 05-1

--- src/task_05.rs
+++ src/task_05.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, error::Error, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+    fs,
+};
 
 const INPUT_PATH: &str = "./inputs/05/input.txt";
 
@@ -22,12 +26,90 @@ fn read_input(path: &str) -> Result<Input, Box<dyn Error>> {
         return Err("Stuff is broken yo.".into());
     };
 
-    // FIXME continue.
-    panic!("Soon to be done.");
+    let page_orderings = page_orderings
+        .lines()
+        .filter_map(|line| -> Option<PageOrdering> {
+            let [before, after] = line
+                .split("|")
+                .filter_map(|item| -> Option<Page> { item.parse::<Page>().ok() })
+                .collect::<Vec<_>>()[..]
+            else {
+                return None;
+            };
+
+            Some(PageOrdering { before, after })
+        })
+        .collect::<Vec<_>>();
+
+    let updates = updates
+        .lines()
+        .map(|line| -> Update {
+            line.split(",")
+                .filter_map(|page| -> Option<Page> { page.parse().ok() })
+                .collect()
+        })
+        .collect::<Vec<_>>();
+
+    Ok((page_orderings, updates))
+}
+
+type BeforeMap = HashMap<Page, HashSet<Page>>;
+
+fn before_map(page_orderings: Vec<PageOrdering>) -> BeforeMap {
+    // We find values that must happen before the others.
+    let mut after_to_befores: BeforeMap = HashMap::new();
+
+    for PageOrdering { before, after } in page_orderings.iter() {
+        match after_to_befores.get_mut(after) {
+            Some(befores) => {
+                befores.insert(*before);
+            }
+            None => {
+                after_to_befores.insert(*after, HashSet::from([*before]));
+            }
+        }
+    }
+
+    return after_to_befores;
+}
+
+fn filter_ordered_updates(after_to_befores: BeforeMap, updates: Vec<Update>) -> Vec<Update> {
+    updates
+        .into_iter()
+        .filter(|update| -> bool {
+            for (index, page) in update.iter().enumerate() {
+                let Some(befores) = after_to_befores.get(page) else {
+                    continue;
+                };
+
+                let afters = &update[index + 1..];
+                if afters.iter().any(|after| befores.contains(after)) {
+                    return false;
+                }
+            }
+
+            return true;
+        })
+        .collect()
+}
+
+fn get_middle(update: Update) -> Page {
+    let index = update.len() / 2;
+    *update.get(index).unwrap_or(&0)
+}
+
+fn solution_1(input: Input) -> Page {
+    let ordered_updates = filter_ordered_updates(before_map(input.0), input.1);
+
+    ordered_updates
+        .into_iter()
+        .map(|update| get_middle(update))
+        .sum::<Page>()
 }
 
 fn first() -> Result<(), Box<dyn Error>> {
-    let wanted = 0;
+    let input = read_input(INPUT_PATH).unwrap();
+    let wanted = solution_1(input);
     println!("{}", wanted);
     Ok(())
 }
@@ -47,11 +129,31 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
+    use super::{before_map, filter_ordered_updates, read_input, solution_1, Update};
+
     const EXAMPLE_PATH: &str = "./inputs/05/example.txt";
 
     #[test]
+    fn should_find_ordered_updates() {
+        let input = read_input(EXAMPLE_PATH).unwrap();
+        let actual = filter_ordered_updates(before_map(input.0), input.1);
+
+        let expected: Vec<Update> = Vec::from([
+            Vec::from([75, 47, 61, 53, 29]),
+            Vec::from([97, 61, 53, 29, 13]),
+            Vec::from([75, 29, 13]),
+        ]);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
     fn should_calculate_first_example() {
-        assert_eq!(1, 0);
+        let input = read_input(EXAMPLE_PATH).unwrap();
+        let actual = solution_1(input);
+        let expected = 143;
+
+        assert_eq!(actual, expected);
     }
 
     #[test]
